635f430e77900e60384937529bbdd401ec623b80
nasteveya <127323790+nasteveya@users.noreply.github.com>
2023-03-09 04:29:29 -0500

    Add files via upload

---
+++ docs/js/javascript.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+function myFunction() {
+    var EC;
+    EC = document.getElementById("EC").innerHTML;
+    console.log(EC);
+}
+
+
+function btnSubmit() {
+    document.getElementById("btnSubmit") = myFunction();
+    console.log(EC);
+}
