b2d3929d3f7044aaf4f2d254831f61bd2af28997
nasteveya <127323790+nasteveya@users.noreply.github.com>
2023-03-10 20:09:16 -0500

    cleaned up error message alert

--- docs/js/javascript.js
+++ docs/js/javascript.js
@@ -16,14 +16,7 @@ var btnSubmit1;
 function getEC() {
     EC = document.getElementById("EC").value;
     console.log(EC.length);
-    if (EC.length == 0)
-    {
-        console.log("Explorer Club Id is null not entered");
-        alert("Please enter an Explorer Club ID.");
-    }
-    else {
-        console.log("Explorer Club Id: " + EC);
-    }
+   
 }
 
 function getTimeZone() {
@@ -33,26 +26,15 @@ function getTimeZone() {
 
 function getDps() {
     countDps = document.getElementById('DPScount').value;
+    console.log("Number of DPS: " + countDps);
     
-    if (countDps == 0) {
-        console.log("DPS count is 0, please choose a higher number.");
-        alert("Please provide at least 1 DPS.");
-    }
-    else {
-        console.log("Number of DPS: " + countDps);
-    }
 
 }
 
 function getSader() {
     countSader = document.getElementById('Sadercount').value;
-    if (countSader == 0) {
-        console.log ("Sader count is 0, please choose a higher number.");
-        alert("Please provide at least 1 Sader.");
-    }
-    else {
-        console.log('Number of Saders: ' + countSader);
-    }
+    console.log('Number of Saders: ' + countSader);
+    
 }
     
 function getTimeZoneTest() {
@@ -63,6 +45,47 @@ function getTimeZoneTest() {
 function sendAlert() {
     //fill this place with one alert message through if else statement
     //instead of sending multiple alert windows
+    if (EC.length == 0 && countDps == 0 && countSader ==0)
+    {
+        //console.log("Explorer Club Id is null not entered");
+        alert("Please enter the following: \n" + 
+        "Explorer Club ID: \n" + 
+        "Number of DPS: \n" +
+        "Number of Sader: \n");
+    }
+    else if(EC.length == 0 && countDps == 0) {
+        alert("Please enter the following: \n" + 
+        "Explorer Club ID: \n" + 
+        "Number of DPS: ");
+    }
+    else if(EC.length == 0 && countSader == 0) {
+        alert("Please enter the following: \n" + 
+        "Explorer Club ID: \n" +
+        "Number of Sader: ");
+    }
+    else if(countDps == 0 && countSader == 0) {
+        alert("Please enter the following: \n" + 
+        "NUmber of DPS: \n" +
+        "Number of Sader: ");
+    }
+    else if(countDps == 0) {
+        alert("Please enter the following: \n" + 
+        "Number of DPS: ");
+    }
+    else if(countSader == 0) {
+        alert("Please enter the following: \n" + 
+        "Number of Sader: ");
+    }
+    else {
+        //need to add timezone error later
+        alert("Following information has been registered: \n" + 
+        "Explorer Club ID: " + EC + "\n" + 
+        "Time Zone: " + timeZone + "\n" +
+        "Time Zone Test: " + timeZone_test + "\n" +
+        "Number of DPS: " + countDps + "\n" + 
+        "Number of Saders: " + countSader);
+        console.log("Error message for later if timezone is not filled out");
+    }
 }
 
 //NEED TO MAKE A FUNCTION TO CONVERT TIME FROM TIME ZONE
@@ -85,12 +108,8 @@ function btnSubmit() {
     getTimeZoneTest();
     getDps();
     getSader();
-    alert("Following information has been registered: \n" + 
-    "Explorer Club ID: " + EC + "\n" + 
-    "Time Zone: " + timeZone + "\n" +
-    "Time Zone Test: " + timeZone_test + "\n" +
-    "Number of DPS: " + countDps + "\n" + 
-    "Number of Saders: " + countSader);
+    sendAlert();
+    
 }
 
 //document ready
